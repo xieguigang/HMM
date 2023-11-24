dd8b82d454b5a6e18cac2a25ea022d837dc72be2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-24 18:43:01 +0800

    add project source

---
+++ Talk/Generator.vb
@@ -0,0 +1,90 @@
+﻿Imports Microsoft.VisualBasic.Linq
+Imports rnd = Microsoft.VisualBasic.Math.RandomExtensions
+
+Public Class Generator
+    Friend previous As String = ""
+    Friend curWord As String = ""
+    Friend numWordsMin As Integer = 2
+    Friend numWordsMax As Integer = 8
+    Public pairs As New Dictionary(Of String, List(Of Word))()
+    Public Sub New()
+
+    End Sub
+    Public Overridable Function generate([string] As String) As String
+        Dim generatedText = ""
+        createTable([string])
+        Dim numWords As Integer = randomizeNumWords()
+        For i = 0 To numWords - 1
+            If generatedText.Equals("") Then
+                Dim keys As New List(Of String)(pairs.Keys)
+
+                curWord = keys(rnd.Next(keys.Count))
+                generatedText += curWord
+            Else
+                generatedText += " "
+                Dim words = pairs(curWord)
+                Dim values = New Double(words.Count - 1) {}
+                For j = 0 To values.Length - 1
+                    values(j) = words(j).num
+                Next
+                If values.Length <> 0 Then
+                    curWord = words(rouletteSelect(values)).s
+                Else
+                    curWord = getRandomWord()
+                End If
+                generatedText += curWord
+            End If
+        Next
+        Return generatedText
+    End Function
+
+    Private Function getRandomWord() As String
+        Dim keys = pairs.Keys.ToArray
+        Dim rndkey = keys(rnd.Next(keys.Length))
+        Dim words = pairs(rndkey)
+        Return words(rnd.Next(words.Count))
+    End Function
+
+    Public Overridable Function rouletteSelect(weight As Double()) As Integer
+        Dim weight_sum As Double = 0
+        For i = 0 To weight.Length - 1
+            weight_sum += weight(i)
+        Next
+        Dim value As Double = rnd.NextDouble() * weight_sum
+        For i = 0 To weight.Length - 1
+            value -= weight(i)
+            If value <= 0 Then
+                Return i
+            End If
+        Next
+        Return weight.Length - 1
+    End Function
+    Public Overridable Function randomizeNumWords() As Integer
+        Return rnd.Next(numWordsMax - numWordsMin + 1) + numWordsMin
+    End Function
+    Public Overridable Sub createTable([string] As String)
+        Dim words = [string].Split(" "c)
+        For i = 0 To words.Length - 1
+            If Not pairs.ContainsKey(words(i)) Then
+                pairs(words(i)) = New List(Of Word)()
+            End If
+            Dim w As List(Of Word) = New List(Of Word)()
+            If pairs.ContainsKey(previous) Then
+                w = pairs(previous)
+                Dim found = False
+                For j = 0 To w.Count - 1
+                    If w(j).s.Equals(words(i)) Then
+                        w(j).num += 1
+                        found = True
+                    End If
+                Next
+                If Not found Then
+                    w.Add(New Word(words(i)))
+                End If
+            End If
+            pairs.Remove(previous)
+            pairs(previous) = w
+            previous = words(i)
+        Next
+    End Sub
+End Class
---
+++ Talk/Manager.vb
@@ -0,0 +1,50 @@
+﻿Imports System
+
+Public Class Manager
+    Private InstanceFieldsInitialized As Boolean = False
+
+    Private Sub InitializeInstanceFields()
+        strings = New String(numSentances - 1) {}
+    End Sub
+
+    Friend numSentances As Integer = 10
+    Friend generator As Generator = New Generator()
+    Friend strings As String()
+    Public Sub New(text As String)
+        If Not InstanceFieldsInitialized Then
+            InitializeInstanceFields()
+            InstanceFieldsInitialized = True
+        End If
+
+        For i = 0 To numSentances - 1
+            strings(i) = generator.generate(text)
+        Next
+        manage()
+        For i = 0 To strings.Length - 1
+            Console.WriteLine(strings(i))
+        Next
+    End Sub
+    Public Overridable Sub manage()
+        For i = 0 To strings.Length - 1
+            Dim [string] = strings(i)
+            If Not ([string].EndsWith("the", StringComparison.Ordinal) OrElse [string].EndsWith("and", StringComparison.Ordinal) OrElse [string].EndsWith("take", StringComparison.Ordinal) OrElse [string].EndsWith("your", StringComparison.Ordinal) OrElse [string].EndsWith("*is*", StringComparison.Ordinal) OrElse [string].EndsWith("each", StringComparison.Ordinal) OrElse [string].EndsWith("they", StringComparison.Ordinal) OrElse [string].EndsWith("like", StringComparison.Ordinal) OrElse [string].EndsWith("in", StringComparison.Ordinal) OrElse [string].EndsWith("my", StringComparison.Ordinal) OrElse [string].EndsWith("his", StringComparison.Ordinal) OrElse [string].EndsWith("our", StringComparison.Ordinal) OrElse [string].EndsWith("a", StringComparison.Ordinal) OrElse [string].EndsWith("she's", StringComparison.Ordinal) OrElse [string].EndsWith("he", StringComparison.Ordinal) OrElse [string].EndsWith(" ", StringComparison.Ordinal) OrElse [string].EndsWith("she", StringComparison.Ordinal) OrElse [string].EndsWith("it", StringComparison.Ordinal) OrElse [string].EndsWith("but", StringComparison.Ordinal) OrElse [string].EndsWith("you're", StringComparison.Ordinal) OrElse [string].EndsWith("on", StringComparison.Ordinal) OrElse [string].EndsWith("or", StringComparison.Ordinal) OrElse [string].EndsWith("introduce", StringComparison.Ordinal) OrElse [string].EndsWith("you", StringComparison.Ordinal) OrElse [string].EndsWith("that's", StringComparison.Ordinal) OrElse [string].EndsWith("their", StringComparison.Ordinal) OrElse [string].EndsWith("ain't", StringComparison.Ordinal) OrElse [string].EndsWith("start", StringComparison.Ordinal) OrElse [string].EndsWith("a Go", StringComparison.Ordinal) OrElse [string].EndsWith("a go", StringComparison.Ordinal) OrElse [string].EndsWith("of", StringComparison.Ordinal) OrElse [string].EndsWith("just", StringComparison.Ordinal) OrElse [string].EndsWith("not", StringComparison.Ordinal) OrElse [string].EndsWith("if", StringComparison.Ordinal) OrElse [string].EndsWith("than", StringComparison.Ordinal) OrElse [string].EndsWith("are", StringComparison.Ordinal) OrElse [string].EndsWith("at", StringComparison.Ordinal) OrElse [string].EndsWith("to", StringComparison.Ordinal) OrElse [string].EndsWith("is", StringComparison.Ordinal) OrElse [string].EndsWith(",", StringComparison.Ordinal) OrElse [string].EndsWith(":", StringComparison.Ordinal) OrElse [string].EndsWith(";", StringComparison.Ordinal) OrElse [string].EndsWith("I'll get", StringComparison.Ordinal) OrElse [string].EndsWith("You'll get", StringComparison.Ordinal) OrElse [string].EndsWith("Don't be", StringComparison.Ordinal) OrElse [string].EndsWith("You're totally", StringComparison.Ordinal) OrElse [string].Matches("^\w*[\.\?\!]").Any) OrElse [string].Matches("[A-Z]+[a-z']+[\.\?\!]*$").Any Then
+                If Not [string].Matches("[\.\?\!\,]$").Any Then
+                    [string] = [string] & "."
+                End If
+                If [string].StartsWith(" ", StringComparison.Ordinal) Then
+                    [string] = [string].Substring(1)
+                End If
+                If [string].StartsWith(" ", StringComparison.Ordinal) Then
+                    [string] = [string].Substring(1)
+                End If
+                If [string].StartsWith(" ", StringComparison.Ordinal) Then
+                    [string] = [string].Substring(1)
+                End If
+                strings(i) = [string].Substring(0, 1).ToUpper() & [string].Substring(1)
+            Else
+                strings(i) = """" & [string] & """"
+            End If
+        Next
+    End Sub
+
+End Class
