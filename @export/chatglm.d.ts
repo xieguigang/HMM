5390cb5c14a4abf11dadc08d13df06ee6f7b8db2
谢桂纲 <gg.xie@bionovogene.com>
2024-04-16 15:19:50 +0800

    run package msbuikld

---
+++ @export/chatglm.d.ts
@@ -0,0 +1,16 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "chatglm" from "Agent";
+//
+// ref=Agent.ChatGLM@Agent, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace chatglm {
+   /**
+   */
+   function history_json(his: object): string;
+   /**
+   */
+   function input_and_response(his: object, input: string, response: string): object;
+}
---
+++ man/Agent/chatglm/chatglm.md
@@ -0,0 +1,6 @@
+﻿# chatglm
+
+
+
++ [input_and_response](chatglm/input_and_response.1) 
++ [history_json](chatglm/history_json.1) 
