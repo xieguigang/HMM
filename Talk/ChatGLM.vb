f62fd311afd2e002659e01e79dff6d2b0875775f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-16 10:34:53 +0800

    add history content helper

---
+++ Talk/ChatGLM.vb
@@ -0,0 +1,42 @@
+﻿
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace ChatGLM
+
+    ''' <summary>
+    ''' helper for create chatglm history
+    ''' </summary>
+    Public Class ChatHistory
+
+        ReadOnly history As New List(Of History)
+
+        Public Function AddAIResponse(content As String) As ChatHistory
+            history.Add(New History With {.role = roles.assistant, .content = content, .metadata = ""})
+            Return Me
+        End Function
+
+        Public Function AddUserInput(content As String) As ChatHistory
+            history.Add(New History With {.role = roles.user, .metadata = Nothing, .content = content})
+            Return Me
+        End Function
+
+        Public Function ToJson() As String
+            Return history.ToArray.GetJson
+        End Function
+
+    End Class
+
+    Public Enum roles
+        user
+        assistant
+    End Enum
+
+    Public Class History
+
+        Public Property role As roles
+        Public Property content As String
+        Public Property metadata As String
+
+    End Class
+
+End Namespace
