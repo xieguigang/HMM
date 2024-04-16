551c3865471ad857d11a6ae57e0e43822896d6e8
谢桂纲 <gg.xie@bionovogene.com>
2024-04-16 15:04:59 +0800

    run package msbuild

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=LLMs
+
+declare namespace LLMs {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
