e42092f23426e2049b6f5f11a49817a2f029993d
谢桂纲 <gg.xie@bionovogene.com>
2024-04-16 15:39:31 +0800

    fix of the json enum serialization

--- src/Talk/ChatGLM.vb
+++ src/Talk/ChatGLM.vb
@@ -11,12 +11,12 @@ Namespace ChatGLM
         ReadOnly history As New List(Of History)
 
         Public Function AddAIResponse(content As String) As ChatHistory
-            history.Add(New History With {.role = roles.assistant, .content = content, .metadata = ""})
+            history.Add(New History With {.role = roles.assistant.Description, .content = content, .metadata = ""})
             Return Me
         End Function
 
         Public Function AddUserInput(content As String) As ChatHistory
-            history.Add(New History With {.role = roles.user, .metadata = Nothing, .content = content})
+            history.Add(New History With {.role = roles.user.Description, .metadata = Nothing, .content = content})
             Return Me
         End Function
 
@@ -33,7 +33,7 @@ Namespace ChatGLM
 
     Public Class History
 
-        Public Property role As roles
+        Public Property role As String
         Public Property content As String
         Public Property metadata As String
 
