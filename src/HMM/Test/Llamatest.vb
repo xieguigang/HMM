7f0a5e1360c4fb6b64fac603c73e4b154737a071
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-03 11:36:26 +0800

    call chat

--- src/HMM/Test/Llamatest.vb
+++ src/HMM/Test/Llamatest.vb
@@ -5,6 +5,11 @@ Module Llamatest
     Sub Main2()
         ' Call Llama2.Run("E:\HMM\data\Llama2\stories15M.bin", tokenizer:="E:\HMM\data\Llama2\tokenizer.bin")
 
+        ' test deepseek
+        Dim result = DeepSeekResponse.Chat("who are you?", "192.168.0.207:11434", "deepseek-r1:1.5b")
+
+
+
         Pause()
     End Sub
 
