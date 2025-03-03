d0ed53f7af67044492e6fdbcccd13e148d37472b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-03 11:38:06 +0800

    deepseek chat

--- src/Agent/Llama.vb
+++ src/Agent/Llama.vb
@@ -1,7 +1,17 @@
 ﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports TalkGenerator
 
-<Package("llama")>
-Module LlamaDemo
+<Package("ollama")>
+Module OLlamaDemo
+
+    <ExportAPI("deepseek_chat")>
+    Public Function deepseek_chat(message As String,
+                                  Optional ollama_serve As String = "127.0.0.1:11434",
+                                  Optional model As String = "deepseek-r1:671b") As DeepSeekResponse
+
+        Return DeepSeekResponse.Chat(message, ollama_serve, model)
+    End Function
 
 End Module
