3804651af04094905dc5d64d3f3a471fe1c8be31
谢桂纲 <gg.xie@bionovogene.com>
2025-03-04 09:53:15 +0800

    run msbuild

--- @export/chatglm.d.ts
+++ @export/chatglm.d.ts
@@ -5,23 +5,54 @@
 // ref=Agent.ChatGLM@Agent, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * 
 */
 declare namespace chatglm {
    /**
-     * @param prompt_text default value Is ``'你是一个从英文到中文的语言翻译器'``.
-     * @param add_explains default value Is ``true``.
-     * @param env default value Is ``null``.
+    * create json data for run chatglm batch task
+    * 
+    * > the required file extension suffix name by upload 
+    * >  batch data from web must be ``jsonl``!
+    * 
+     * @param content a dataframe object that contains the batch task data for run:
+     *  
+     *  1. id: the unique reference id of each task
+     *  2. term: the term for do translation
+     * @param prompt_text 
+     * + default value Is ``'你是一个从英文到中文的语言翻译器'``.
+     * @param add_explains 
+     * + default value Is ``true``.
+     * @param env 
+     * + default value Is ``null``.
+     * @return a collection of the batch task data for run the request
    */
    function batch_transaltion(content: object, prompt_text?: string, add_explains?: boolean, env?: object): object;
    /**
+    * build json string for chatglm history input
+    * 
+    * 
+     * @param his -
    */
    function history_json(his: object): string;
    /**
+    * add a new record of chat input and ai response for create the history data
+    * 
+    * 
+     * @param his -
+     * @param input -
+     * @param response -
    */
    function input_and_response(his: object, input: string, response: string): object;
    /**
-     * @param parse_annotation default value Is ``true``.
-     * @param env default value Is ``null``.
+    * parse the result of the chatglm batch request
+    * 
+    * 
+     * @param file -
+     * @param parse_annotation 
+     * + default value Is ``true``.
+     * @param env -
+     * 
+     * + default value Is ``null``.
    */
    function parse_batch_output(file: any, parse_annotation?: boolean, env?: object): any;
 }
--- man/Agent/chatglm/chatglm.md
+++ man/Agent/chatglm/chatglm.md
@@ -2,7 +2,7 @@
 
 
 
-+ [input_and_response](chatglm/input_and_response.1) 
-+ [history_json](chatglm/history_json.1) 
-+ [batch_transaltion](chatglm/batch_transaltion.1) 
-+ [parse_batch_output](chatglm/parse_batch_output.1) 
++ [input_and_response](chatglm/input_and_response.1) add a new record of chat input and ai response for create the history data
++ [history_json](chatglm/history_json.1) build json string for chatglm history input
++ [batch_transaltion](chatglm/batch_transaltion.1) create json data for run chatglm batch task
++ [parse_batch_output](chatglm/parse_batch_output.1) parse the result of the chatglm batch request
