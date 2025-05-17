59464aa156ecbe45ec272e210c8844b3f8d83a51
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-17 23:59:44 +0800

    fix of the missing code span reference

--- src/Agent/OLlama.vb
+++ src/Agent/OLlama.vb
@@ -1,10 +1,15 @@
 ﻿
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Ollama
+Imports Ollama.JSON.FunctionCall
+Imports SMRUCC.Rsharp.Runtime
 Imports SMRUCC.Rsharp.Runtime.Components.[Interface]
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.Rsharp.Runtime.Vectorization
+Imports RInternal = SMRUCC.Rsharp.Runtime.Internal
 
 <Package("ollama")>
 Module OLlamaDemo
@@ -21,11 +26,38 @@ Module OLlamaDemo
 
     <ExportAPI("add_tool")>
     Public Function add_tool(model As Ollama.Ollama, name$, desc$,
-                             <RByRefValueAssign> fcall As RFunction,
-                             <RListObjectArgument>
                              Optional args As list = Nothing,
+                             <RRawVectorArgument>
+                             Optional requires As Object = Nothing,
+                             <RByRefValueAssign>
+                             Optional fcall As RFunction = Nothing,
                              Optional env As Environment = Nothing) As Object
 
+        Dim argList As New Dictionary(Of String, ParameterProperties)
+
+        If fcall Is Nothing Then
+            Return RInternal.debug.stop("Missing function value for make LLMs tool call!", env)
+        End If
+        If Not args Is Nothing Then
+            For Each arg As String In args.getNames
+                Call argList.Add(arg, New ParameterProperties With {
+                    .name = arg,
+                    .description = CLRVector.asCharacter(args.getByName(arg)).JoinBy(vbCrLf)
+                })
+            Next
+        End If
+
+        Dim f As New FunctionModel With {
+            .name = name,
+            .description = desc,
+            .parameters = New FunctionParameters With {
+                .required = CLRVector _
+                    .asCharacter(requires) _
+                    .SafeQuery _
+                    .ToArray,
+                .properties = argList
+            }
+        }
     End Function
 
     ' start server
