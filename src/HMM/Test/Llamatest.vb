097774417f05be6184ac6044b914eafe1e661c19
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-17 13:18:24 +0800

    config run test

--- src/HMM/Test/Llamatest.vb
+++ src/HMM/Test/Llamatest.vb
@@ -7,14 +7,15 @@ Module Llamatest
     Sub Main2()
         ' Call Llama2.Run("E:\HMM\data\Llama2\stories15M.bin", tokenizer:="E:\HMM\data\Llama2\tokenizer.bin")
 
-        Dim test_parse = DeepSeekResponse.ParseResponse(DeepSeekResponse.who_are_you)
-        Dim list As String() = "E:\HMM\test\stream.jsonl".ReadAllLines
+        '  Dim test_parse = DeepSeekResponse.ParseResponse(DeepSeekResponse.who_are_you)
+        '  Dim list As String() = "E:\HMM\test\stream.jsonl".ReadAllLines
 
 
         ' test deepseek
-        Dim result = DeepSeekResponse.Chat("who are you?", "192.168.0.207:11434", "deepseek-r1:1.5b")
+        Dim result = DeepSeekResponse.Chat("who are you?", "127.0.0.1:11434", "deepseek-r1:32b")
 
-
+        Call Console.WriteLine(result.think)
+        Call Console.WriteLine(result.output)
 
         Pause()
     End Sub
