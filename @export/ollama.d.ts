ca59331111e0534b1d0ea8c950a27e612d8b50f8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-17 22:08:55 +0800

    export chat api

--- @export/ollama.d.ts
+++ @export/ollama.d.ts
@@ -8,8 +8,15 @@
 */
 declare namespace ollama {
    /**
+   */
+   function chat(model: object, msg: string): any;
+   /**
      * @param ollama_serve default value Is ``'127.0.0.1:11434'``.
      * @param model default value Is ``'deepseek-r1:671b'``.
    */
    function deepseek_chat(message: string, ollama_serve?: string, model?: string): object;
+   /**
+     * @param ollama_server default value Is ``'127.0.0.1:11434'``.
+   */
+   function new(model: string, ollama_server?: string): object;
 }
--- man/Agent/ollama/ollama.md
+++ man/Agent/ollama/ollama.md
@@ -2,4 +2,6 @@
 
 
 
++ [new](ollama/new.1) 
++ [chat](ollama/chat.1) 
 + [deepseek_chat](ollama/deepseek_chat.1) 
