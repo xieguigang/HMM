ad7db501bdb6c0582f5dd7ac1fb58a7e0fc45275
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-19 16:28:14 +0800

    create jhelper functions

---
+++ R/Ragent/read.csv.R
@@ -0,0 +1,3 @@
+const agent_readcsv = function() {
+    
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 imports "chatglm" from "Agent";
 
 const .onLoad = function() {
-    
+    print("LLMs agent for R# language programming");
 }
